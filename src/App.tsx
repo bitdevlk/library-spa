3c29af65fc5a50d8de2ae2d3030ff7eabf4887fb
HasinduLanka <hlrathnayaka@gmail.com>
2022-01-13 16:34:30 +0530

    Add bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Row';
 import './App.css';
 
 function App() {
   return (
     <div>
       Hello world
+      <input type="button" className="col btn btn-primary" value="Hello" />
     </div>
   );
 }
