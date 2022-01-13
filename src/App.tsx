fd7522d2abb4832697b456746825372e8690f956
HasinduLanka <hlrathnayaka@gmail.com>
2022-01-13 17:49:20 +0530

    Main layout and Hero section done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Row';
 import './App.css';
+import Books from './Books';
+import Authors from './Authors';
 
 function App() {
   return (
-    <div>
-      Hello world
-      <input type="button" className="col btn btn-primary" value="Hello" />
+    <div className='row' >
+      <div className='col-12 text-center mt-3'>
+        <h2 className='hero-title' > My Library </h2>
+      </div>
+      <div className='col-12 border-0 px-0 my-2' >
+        <img src="library.webp" alt="library" className='img-fluid border-0' />
+      </div>
+      <div className='col-12 my-2 text-end' >
+        <a className='text-reset text-decoration-none' href="https://unsplash.com/photos/ajE5goOGzZc" >
+          <h4 className='px-5'> photo credits </h4>
+        </a>
+      </div>
+      <div className='col-6' >
+        <Books></Books>
+      </div>
+      <div className='col-6' >
+        <Authors></Authors>
+      </div>
     </div>
   );
 }
---
+++ src/Books.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from "react";
+
+
+const Books: FunctionComponent = () => {
+    return (<div >
+        <h1>Books</h1>
+    </div>);
+}
+
+export default Books;
