f458698a1c5e32712d386d25f9b169f1d4621bb0
HasinduLanka <hlrathnayaka@gmail.com>
2022-01-16 13:51:43 +0530

    Font size calc funcs

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,13 @@ function App() {
   return (
     <div className='row' >
       <div className='col-12 text-center mt-3'>
-        <h2 className='hero-title' > My Library </h2>
+        <h1 className='hero-title' > My Library </h1>
       </div>
       <div className='col-12 border-0 px-0 my-2' >
         <img src="library.webp" alt="library" className='img-fluid border-0' />
       </div>
       <div className='col-12 my-2 text-end' >
-        <h4 className='credit-text'> Photo by <a href="https://unsplash.com/@annahunko?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Anna Hunko</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> </h4>
+        <p> <span className='credit-text'> Photo by <a href="https://unsplash.com/@annahunko?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Anna Hunko</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> </span> </p>
       </div>
       <div className='col-6' >
         <Books></Books>
