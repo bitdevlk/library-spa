38fb6225e0733735826e53b819a821a9126a06fd
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-01-25 22:19:18 +0530

    Author welcome done

---
+++ src/components/Author/Authors.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from "react";
+import AuthorWelcome from "./AuthorWelcome";
+
+
+const Authors: FunctionComponent = () => {
+    return (
+        <div className="mx-3">
+            <AuthorWelcome />
+        </div>
+    );
+}
+
+export default Authors;
