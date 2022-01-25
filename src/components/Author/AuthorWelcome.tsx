05207f570290cd4695b7b8d4bd4ddf0ae71b909b
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-01-25 22:19:18 +0530

    Author welcome done

---
+++ src/components/Author/AuthorWelcome.tsx
@@ -0,0 +1,10 @@
+import {FunctionComponent} from "react";
+
+const AuthorWelcome: FunctionComponent = () => {
+    return (<>
+            <p className="h1">Authors</p>
+            <hr className="py-0 mt-1"/>
+        </>
+    )
+}
+export default AuthorWelcome;
