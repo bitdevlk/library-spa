4b06296f715bab153ad2180b14aa4bc72d357a58
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-01-26 22:24:30 +0530

    Added empty author component

--- src/components/Author/AuthorList.tsx
+++ src/components/Author/AuthorList.tsx
@@ -1,11 +1,13 @@
 import {FunctionComponent} from "react";
 import AuthorListItem from "./AuthorListItem";
 import {Col, Container, Row} from "react-bootstrap";
+import EmptyAuthor from "./EmptyAuthor";
 
 const AuthorList: FunctionComponent = () => {
     return (
         <Row className="mx-0 px-0 my-3">
             <Col className="">
+                <EmptyAuthor />
                 <AuthorListItem/>
                 <AuthorListItem/>
                 <AuthorListItem/>
---
+++ src/components/Author/EmptyAuthor.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from "react";
+
+const EmptyAuthor:FunctionComponent =  () => {
+    return(
+        <>
+            <p className="fst-italic fs-5">No Authors Listed Here</p>
+        </>
+    );
+}
+export default EmptyAuthor;
