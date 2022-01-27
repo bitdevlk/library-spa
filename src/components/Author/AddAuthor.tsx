fa703bd1e068a14c3b3e17d937c505aee47cf396
HasinduLanka <hlrathnayaka@gmail.com>
2022-01-27 07:20:33 +0530

    Cleanup duplicate books dir Lint codes

--- src/components/Author/AddAuthor.tsx
+++ src/components/Author/AddAuthor.tsx
@@ -1,15 +1,15 @@
-import React, {FunctionComponent, PropsWithChildren} from "react";
-import {Col, Row} from "react-bootstrap";
-import {Plus} from "react-feather";
+import React, { FunctionComponent, PropsWithChildren } from "react";
+import { Col, Row } from "react-bootstrap";
+import { Plus } from "react-feather";
 type AddAuthorPropsModel = {
     handleOnAddAuthorClick: () => void;
 }
-const AddAuthor:React.FC<AddAuthorPropsModel> = (props:PropsWithChildren<AddAuthorPropsModel>) => {
-    const  { handleOnAddAuthorClick } = props;
-    return(
+const AddAuthor: React.FC<AddAuthorPropsModel> = (props: PropsWithChildren<AddAuthorPropsModel>) => {
+    const { handleOnAddAuthorClick } = props;
+    return (
         <Row className="my-4 addAuthorSection">
             <Col className="px-0 d-flex align-items-center ">
-                <Plus className="addIcon" onClick={handleOnAddAuthorClick}/>
+                <Plus className="addIcon" onClick={handleOnAddAuthorClick} />
                 <div className="d-inline fs-5 ms-1 " onClick={handleOnAddAuthorClick}>Add Author</div>
             </Col>
         </Row>
