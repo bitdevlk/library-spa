232ff4c5b546b56b156558265e6d8eac5c273297
HasinduLanka <hlrathnayaka@gmail.com>
2022-01-28 21:54:46 +0530

    Fix opacity of <hr> Book combo box visual fix

--- src/components/Author/AuthorWelcome.tsx
+++ src/components/Author/AuthorWelcome.tsx
@@ -6,7 +6,7 @@ const AuthorWelcome: FunctionComponent = () => {
         <Row className="px-0 col-sm-12">
             <Col className="px-0">
                 <p className="h1">Authors</p>
-                <hr className="py-0 mb-3 mt-1" />
+                <hr className="py-0 mb-3 mt-1 opacity-100" />
             </Col>
 
         </Row>
--- src/components/Book/BooksWelcome.tsx
+++ src/components/Book/BooksWelcome.tsx
@@ -6,7 +6,7 @@ const BooksWelcome: FunctionComponent = () => {
         <Row className="px-0 col-sm-12">
             <Col className="px-0">
                 <h1 className="h1">Books</h1>
-                <hr className="py-0 mb-3 mt-1" />
+                <hr className="py-0 mb-3 mt-1 opacity-100" />
             </Col>
 
         </Row>
