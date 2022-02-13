c4e866a4ed6510f1d47535d516bd3e9b7cf4b9c1
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-02-13 20:07:01 +0530

    completed welcome section

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -8,9 +8,12 @@ const Welcome: React.FC = () => {
             <img src={WelcomeImage} className="img-fluid" alt="hero-image"/>
             <h5 className="photo-credits pe-md-5 pe-2 py-md-1 py-1">
                 Photo by
-                <a href="https://unsplash.com/@annahunko?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"> Anna Hunko
-                </a> on
-                <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"> Unsplash
+                <a
+                    href="https://unsplash.com/@annahunko?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                    {" "}Anna Hunko
+                </a> on{" "}
+                <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                    Unsplash
                 </a>
             </h5>
         </React.Fragment>
